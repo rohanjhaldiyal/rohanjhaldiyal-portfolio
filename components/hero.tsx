3645bd1abc2b4c46928faf1478d88ed57f641658
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2024-07-13 12:01:21 +0530

    refactor: Update Hero component with improved content and design

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,10 +8,7 @@ export default function Hero() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full">
       <Avatar className="h-40 w-40">
-        <AvatarImage
-          src="/rohanjhaldiyal.jpg"
-          alt="rohanjhaldiyal"
-        />
+        <AvatarImage src="/rohanjhaldiyal.jpg" alt="rohanjhaldiyal" />
         <AvatarFallback>RJ</AvatarFallback>
       </Avatar>
       <h1 className="text-xl text-center text-sky-400 font-normal mt-10 uppercase">
@@ -22,19 +19,19 @@ export default function Hero() {
         <span className="text-sky-300">Beautifully Crafted Web-Apps.</span>
       </h3>
       <h3 className="text-lg text-center text-gray-300 font-normal mt-2">
-        A <span className="text-sky-300">Full Stack Developer</span> by Passion
-        and an <span className="text-sky-300">ML-Engineer</span> by Profession.
+        A <span className="text-sky-300">Full Stack Developer</span> currently{" "}
+        <span className="text-sky-300">Open to Work</span>{" "}
+        (Internships/Contract/Full-Time).
       </h3>
       <h3 className="text-lg text-center text-gray-300 font-normal mt-2">
-        I <span className="text-sky-300">Freelance</span> part time and{" "}
-        <span className="text-sky-300">
-          Create Websites for Businesses and Individuals.
-        </span>
+        I like creating{" "}
+        <span className="text-sky-300">Elegant Designs for Websites</span> in my
+        free time.
       </h3>
       <Socials />
       <div className="text-center block md:hidden text-gray-400 text-xs p-2 mt-4">
         <Link href="#Skills">
-        Scroll to see more <ArrowDownIcon className="h-4 w-4 inline-block" />
+          Scroll to see more <ArrowDownIcon className="h-4 w-4 inline-block" />
         </Link>
       </div>
     </div>
